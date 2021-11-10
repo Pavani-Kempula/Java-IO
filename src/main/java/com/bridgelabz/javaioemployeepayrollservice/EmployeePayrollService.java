8e4da187b9999b0186cdd20e87f38f90b5434be0
PavaniKempula <pavanikempula1998@gmail.com>
2021-11-10 23:13:51 +0530

    ADDED : Welcome to Java IO Program

---
+++ src/main/java/com/bridgelabz/javaioemployeepayrollservice/EmployeePayrollService.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.javaioemployeepayrollservice;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        // Displaying Welcome Message
+        System.out.println("Welcome to Java IO Program");
+    }
+}
